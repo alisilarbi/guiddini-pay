bca8c8481986b563c860128de68f4c2fd4da5458
da-ci <ali.silarbi00@gmail.com>
2025-02-10 12:18:11 +0100

    working on execute paymetn

--- app/Services/OnlinePaymentService.php
+++ app/Services/OnlinePaymentService.php
@@ -37,15 +37,19 @@ class OnlinePaymentService
         $orderId = $transaction->client_order_id;
 
         $returnUrl = route('confirm') . '?orderNumber=' . $orderId . '&bool=0';
+
         $jsonParams = json_encode([
             "orderNumber" => $orderId,
             "udf1" => $orderId,
             "udf5" => "00",
-            "force_terminal_id" => "E010901161"
+            "force_terminal_id" => "E010901161ss"
             // "force_terminal_id" => "HEHEHEHE",
         ]);
 
-        $response = Http::asForm()
+
+        $response = Http::timeout(60)
+            // ->withOptions(['verify' => true])
+            ->asForm()
             ->post($paymentGatewayUrl, [
                 "userName" => $username,
                 "password" => $password,
@@ -58,6 +62,8 @@ class OnlinePaymentService
 
         $result = $response->json();
 
+        dd($result);
+
         return $result;
 
         if (isset($result['errorCode']) && strval($result['errorCode']) === "0") {
@@ -67,6 +73,65 @@ class OnlinePaymentService
             return redirect("https://efawtara.com/?MessageReturn=" . urlencode($errorMessage));
         }
     }
+
+
+    // public function execute(Request $request)
+    // {
+    //     $request->validate([
+    //         'pack_name' => 'required',
+    //         'price' => 'required',
+    //         'name' => 'required',
+    //         'email' => 'required',
+    //         'phone' => 'required',
+    //         'client_order_id' => 'required',
+    //     ]);
+
+    //     $transaction = Transaction::create([
+    //         'pack_name' => $request->pack_name,
+    //         'price' => $request->price,
+    //         'name' => $request->name,
+    //         'email' => $request->email,
+    //         'phone' => $request->phone,
+    //         'client_order_id' => $request->client_order_id,
+    //     ]);
+
+    //     $paymentGatewayUrl = "https://test.satim.dz/payment/rest/register.do";
+
+    //     $username = 'SAT2405190928';
+    //     $password = 'satim120';
+
+    //     $orderId = $transaction->client_order_id;
+    //     $returnUrl = route('confirm', ['orderNumber' => $orderId, 'bool' => 0]);
+
+    //     $jsonParams = json_encode([
+    //         "orderNumber" => $orderId,
+    //         "udf1" => $orderId,
+    //         "udf5" => "00",
+    //         "force_terminal_id" => "E010901161"
+    //     ]);
+
+    //     $response = Http::asForm()->post($paymentGatewayUrl, [
+    //         "sslverify" => "true",
+    //         "timeout" => 60,
+    //         "userName" => $username,
+    //         "password" => $password,
+    //         "returnUrl" => $returnUrl,
+    //         "orderNumber" => $orderId,
+    //         "amount" => $request->price * 100,
+    //         "currency" => "012",
+    //         "jsonParams" => $jsonParams
+    //     ]);
+
+    //     $result = $response->json();
+
+    //     return $result;
+
+    //     if (isset($result['errorCode']) && strval($result['errorCode']) === "0") {
+    //         return redirect($result['formUrl']);
+    //     } else {
+    //         return redirect("https://efawtara.com/?MessageReturn=" . ($result['errorMessage'] ?? 'Unknown error'));
+    //     }
+    // }
 
     public function confirm()
     {
