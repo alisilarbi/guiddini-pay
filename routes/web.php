306d33b67bd8f8cde597bf5cdcd715c16538244d
da-ci <ali.silarbi00@gmail.com>
2025-02-10 14:44:35 +0100

    tsting the payment on the server

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\OnlinePaymentService;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/payment/confirm', [OnlinePaymentService::class, 'confirm'])->name('payment.confirm');
+Route::get('/payment/failed', [OnlinePaymentService::class, 'failed'])->name('payment.failed');
