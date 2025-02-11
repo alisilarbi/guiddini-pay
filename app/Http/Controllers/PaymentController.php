8afb5896408bc04a5edbcdeb7ab7267eaf5f9226
da-ci <ali.silarbi00@gmail.com>
2025-02-11 11:23:30 +0100

    new version of payment service

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -83,7 +83,7 @@ class PaymentController extends Controller
         // $transaction = Transaction::where('client_order_id', $clientOrderId)->first();
         // return view('payment.failed', compact('transaction'));
 
-        dd($request->headers);
+        dd($request->all());
 
         try {
             $result = $this->paymentService->confirmPayment(
