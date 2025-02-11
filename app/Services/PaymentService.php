831a68f02bf98888e9067155a8b551d12b3ae91c
da-ci <ali.silarbi00@gmail.com>
2025-02-11 11:56:58 +0100

    new version of payment service

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -96,7 +96,7 @@ class PaymentService
             $response = Http::timeout(30)->get($this->gatewayUrl . 'confirmOrder.do', $params);
             $result = $response->json();
 
-            dd($result);
+            // dd($result);
             $this->updateTransactionStatus($transaction, $result);
 
             return [
