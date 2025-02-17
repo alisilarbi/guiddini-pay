f5a03ee6f058e406783d55c6498864512f34f8e0
da-ci <ali.silarbi00@gmail.com>
2025-02-17 09:12:52 +0100

    real time tests

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -58,7 +58,7 @@ class PaymentService
         ];
 
         $response = Http::timeout(30)->get($this->gatewayUrl . 'register.do', $params);
-        dd($response);
+        dd($response->json());
         if ($response->successful()) {
             $transaction->update([
                 'gateway_order_id' => $response->json('orderId'),
