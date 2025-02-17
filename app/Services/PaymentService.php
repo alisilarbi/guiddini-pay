3eeb8828f44be945fed9ece885f58ae1c011e0f0
da-ci <ali.silarbi00@gmail.com>
2025-02-17 10:58:21 +0100

    real time tests

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -17,7 +17,7 @@ class PaymentService
     {
         $application = Application::where('app_key', $appKey)->first();
 
-        dd($application->environment);
+        dd($application->env());
 
         $transaction = $this->createTransaction($data, $application);
         $response = $this->callPaymentGateway($transaction, $application);
