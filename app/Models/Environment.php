5f5392bc45b96882510b9236bae34c1730dc6363
da-ci <ali.silarbi00@gmail.com>
2025-02-17 10:57:59 +0100

    real time tests

--- app/Models/Environment.php
+++ app/Models/Environment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Application;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
@@ -26,6 +27,11 @@ class Environment extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
     }
 
 }
