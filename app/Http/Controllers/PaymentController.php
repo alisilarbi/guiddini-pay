ccb7f32f84dd71c2f028c2d3e1f7ed79ca6442c3
da-ci <ali.silarbi00@gmail.com>
2025-02-18 09:19:34 +0100

    real time tests

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -66,7 +66,6 @@ class PaymentController extends Controller
     public function failed(Request $request)
     {
 
-        dd($request->all());
         $result = $this->paymentService->confirmPayment(
             $request->orderId,
         );
