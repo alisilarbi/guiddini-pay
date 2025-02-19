9eb6916cb41d54578ac885b0a0ac020341239705
da-ci <ali.silarbi00@gmail.com>
2025-02-19 09:37:32 +0100

    working on unique order number

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -80,7 +80,7 @@ class PaymentService
         $response = Http::timeout(30)->get($this->gatewayUrl . 'register.do', $params);
         if ($response->successful())
         {
-            dd('hehe');
+            dd($response);
             $transaction->update([
                 'order_id' => $response->json('orderId'),
                 'status' => $response->json('errorCode') == 0 ? 'pending_confirmation' : 'gateway_error'
