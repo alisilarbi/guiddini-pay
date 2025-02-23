1b3bac81b5f09eef0a689f1e1ae2e34fb00f55e4
da-ci <ali.silarbi00@gmail.com>
2025-02-23 11:16:53 +0100

    some changes to the architecture

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -40,7 +40,7 @@ class PaymentController extends Controller
         $result = $this->paymentService->confirmPayment($request->orderId);
         $formattedResponse = $this->formatResponse($result);
 
-        dd($result);
+        dd($result['transaction']->environment);
     }
 
     protected function formatResponse(array $result)
