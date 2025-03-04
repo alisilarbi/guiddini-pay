67a3703ff9093546638a9176831200245109b3e3
da-ci <ali.silarbi00@gmail.com>
2025-03-04 11:06:43 +0100

    refixed storage folder and added new columns to trasnactons table

--- app/Services/Payments/TransactionUpdater.php
+++ app/Services/Payments/TransactionUpdater.php
@@ -52,34 +52,34 @@ class TransactionUpdater
 
         // $transaction->update($updateData);
 
-        // $transaction->update([
-        //     'deposit_amount' => $updateData['desposit_amount'],
-        //     'auth_code' => $updateData['auth_code'],
-        //     'action_code' => $updateData['action_code'],
-        //     'action_code_description' => $updateData['action_code_description'],
-        //     'status' => $updateData['status'],
-        //     'svfe_response' => $updateData['svfe_response'],
-        //     'pan' => $updateData['pan'],
-        //     'ip_address' => $updateData['ip_address'],
-        //     'confirmation_status' => $updateData['confirmation_status'],
-        // ]);
+        $transaction->update([
+            'deposit_amount' => $updateData['deposit_amount'],
+            'auth_code' => $updateData['auth_code'],
+            'action_code' => $updateData['action_code'],
+            'action_code_description' => $updateData['action_code_description'],
+            'status' => $updateData['status'],
+            'svfe_response' => $updateData['svfe_response'],
+            'pan' => $updateData['pan'],
+            'ip_address' => $updateData['ip_address'],
+            'confirmation_status' => $updateData['confirmation_status'],
+        ]);
 
-        try {
-            $transaction->update([
-                'deposit_amount' => $updateData['desposit_amount'],
-                'auth_code' => $updateData['auth_code'],
-                'action_code' => $updateData['action_code'],
-                'action_code_description' => $updateData['action_code_description'],
-                'status' => $updateData['status'],
-                'svfe_response' => $updateData['svfe_response'],
-                'pan' => $updateData['pan'],
-                'ip_address' => $updateData['ip_address'],
-                'confirmation_status' => $updateData['confirmation_status'],
-            ]);
-            dd($transaction);
-        } catch (\Exception $e) {
-            dd($e->getMessage(), $e->getTraceAsString());
-        }
+        // try {
+        //     $transaction->update([
+        //         'deposit_amount' => $updateData['desposit_amount'],
+        //         'auth_code' => $updateData['auth_code'],
+        //         'action_code' => $updateData['action_code'],
+        //         'action_code_description' => $updateData['action_code_description'],
+        //         'status' => $updateData['status'],
+        //         'svfe_response' => $updateData['svfe_response'],
+        //         'pan' => $updateData['pan'],
+        //         'ip_address' => $updateData['ip_address'],
+        //         'confirmation_status' => $updateData['confirmation_status'],
+        //     ]);
+        //     dd($transaction);
+        // } catch (\Exception $e) {
+        //     dd($e->getMessage(), $e->getTraceAsString());
+        // }
 
         dd($transaction);
     }
