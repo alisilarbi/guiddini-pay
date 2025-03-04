32c68a7ef78a7cf86fd13758f5411b92108d7857
da-ci <ali.silarbi00@gmail.com>
2025-03-04 11:56:06 +0100

    added fetch transaction

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -71,6 +71,7 @@ class PaymentController extends Controller
 
     public function getTransaction(Request $request)
     {
+        dd($request->all());
         try {
             $transaction = Transaction::where('order_number', $request->order_number)->findOrFail();
 
