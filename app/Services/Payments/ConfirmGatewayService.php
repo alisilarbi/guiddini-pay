26a59855a9fa05c568c7fb50e7c1ff94524bd427
da-ci <ali.silarbi00@gmail.com>
2025-03-04 10:53:20 +0100

    refixed storage folder and added new columns to trasnactons table

--- app/Services/Payments/ConfirmGatewayService.php
+++ app/Services/Payments/ConfirmGatewayService.php
@@ -33,6 +33,8 @@ class ConfirmGatewayService
                 ->throw()
                 ->json();
 
+            dd($response);
+
             $this->updater->handleConfirmationResponse($transaction, $response);
 
             dd($transaction);
