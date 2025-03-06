4712a5229e09fc7bb6b5a91995140b7b93fb1fa9
da-ci <ali.silarbi00@gmail.com>
2025-03-06 13:29:34 +0100

    added transactions table to admin

--- app/Filament/Pages/Admin/Transactions.php
+++ app/Filament/Pages/Admin/Transactions.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Pages\Admin;
 
 use Filament\Pages\Page;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Table;
+use App\Models\Transaction;
 
+use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Tables\Concerns\InteractsWithTable;
 
@@ -23,45 +26,21 @@ class Transactions extends Page implements HasForms, HasTable
         return $table
             ->query(Transaction::query())
             ->columns([
-                TextColumn::make('pack_name')
-                    ->label('Ref Produit'),
 
-                TextColumn::make('price')
-                    ->label('Prix'),
-
-                TextColumn::make('name')
-                    ->label('Nom du client'),
-
-                TextColumn::make('email')
-                    ->label('Email'),
-
-                TextColumn::make('phone')
-                    ->label('Téléphone'),
-
-                TextColumn::make('client_order_id'),
-
-                TextColumn::make('gateway_order_id'),
-
-                TextColumn::make('gateway_bool'),
-
-                TextColumn::make('gateway_response_message'),
-
-                TextColumn::make('gateway_error_code'),
-
-                TextColumn::make('gateway_code'),
-
-                TextColumn::make('gateway_code'),
-
+                TextColumn::make('amount'),
+                TextColumn::make('order_id'),
+                TextColumn::make('order_number'),
+                TextColumn::make('action_code_description'),
+                TextColumn::make('action_code'),
+                TextColumn::make('status'),
+                TextColumn::make('form_url'),
+                TextColumn::make('ip_address'),
+                TextColumn::make('updated_at'),
 
             ])
-            ->filters([
-            ])
-            ->actions([
-            ])
-            ->headerActions([
-            ])
-            ->bulkActions([
-            ]);
+            ->filters([])
+            ->actions([])
+            ->headerActions([])
+            ->bulkActions([]);
     }
-
 }
