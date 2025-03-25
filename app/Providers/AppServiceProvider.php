67e94bfc534ca35b1ded5da3da7c7f9379055049
da-ci <ali.silarbi00@gmail.com>
2025-03-25 13:55:05 +0100

    Prohibiting destructive commands

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,19 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Prohibitable;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Console\WipeCommand;
+use Illuminate\Database\Console\Migrations\FreshCommand;
+use Illuminate\Database\Console\Migrations\ResetCommand;
+use Illuminate\Database\Console\Migrations\RefreshCommand;
 
 class AppServiceProvider extends ServiceProvider
 {
+    use Prohibitable;
+
     /**
      * Register any application services.
      */
@@ -21,5 +29,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        DB::prohibitDestructiveCommands($this->app->isProduction());
     }
 }
