3034094591367593b9f5008e40d85d5685fe3c35
da-ci <ali.silarbi00@gmail.com>
2025-03-25 10:49:37 +0100

    fixing some bugs

--- app/Filament/Partner/Pages/Applications.php
+++ app/Filament/Partner/Pages/Applications.php
@@ -46,6 +46,12 @@ class Applications extends Page implements HasForms, HasTable
 
     protected static ?string $navigationGroup = 'Integrations';
 
+    public function mount():void
+    {
+        $user = Auth::user()->id;
+        dd($user);
+    }
+
     public function table(Table $table): Table
     {
         return $table
@@ -321,8 +327,11 @@ class Applications extends Page implements HasForms, HasTable
 
                     ])
                     ->action(function (array $data) {
+                        // $user = Auth::user();
+
 
                         $application = Application::create([
+                            // 'partner_id' => Auth::user()->partner->id
                             'name' => $data['name'],
                             'website_url' => $data['website_url'],
                             'redirect_url' => $data['redirect_url'],
