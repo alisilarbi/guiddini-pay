e5b8848a40407f668bcf893c4266bcbb18310291
da-ci <ali.silarbi00@gmail.com>
2025-04-08 13:44:46 +0100

    new prospect email

--- resources/views/mail/partner/new-prospect-registered.blade.php
+++ resources/views/mail/partner/new-prospect-registered.blade.php
@@ -1,19 +1,24 @@
 @component('mail::message')
     # New Prospect Created
 
-    A new prospect has been added.
-
     **Name:** {{ $prospect->name }}
     **Company Name:** {{ $prospect->company_name }}
     **Phone:** {{ $prospect->phone }}
     **Email:** {{ $prospect->email }}
     **Legal Status:** {{ $prospect->legal_status }}
     **Has Bank Account:** {{ $prospect->has_bank_account ? 'Yes' : 'No' }}
-    @if ($prospect->has_bank_account)
+    @if ($prospect->has_bank_account && $prospect->bank_name)
         **Bank Name:** {{ $prospect->bank_name }}
     @endif
-    **Website URL:** {{ $prospect->website_url }}
-    **Programming Languages:** {{ implode(', ', $prospect->programming_languages ?? []) }}
+    @if ($prospect->website_url)
+        **Website URL:** {{ $prospect->website_url }}
+    @endif
+    @if (!empty($prospect->programming_languages))
+        **Programming Languages:**
+        {{ is_array($prospect->programming_languages) ? implode(', ', $prospect->programming_languages) : $prospect->programming_languages }}
+    @endif
     **Needs Help:** {{ $prospect->needs_help ? 'Yes' : 'No' }}
-    **Reference:** {{ $prospect->reference }}
+    @if ($prospect->reference)
+        **Reference:** {{ $prospect->reference }}
+    @endif
 @endcomponent
