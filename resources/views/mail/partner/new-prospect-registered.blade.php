044f3ae076e33863999be10bc8f55fcb42f8675a
da-ci <ali.silarbi00@gmail.com>
2025-04-15 16:24:38 +0100

    fixing email

--- resources/views/mail/partner/new-prospect-registered.blade.php
+++ resources/views/mail/partner/new-prospect-registered.blade.php
@@ -8,13 +8,13 @@
     Legal Status: {{ $prospect->legal_status }}
     Has Bank Account: {{ $prospect->has_bank_account ? 'Yes' : 'No' }}
     @if ($prospect->has_bank_account && $prospect->bank_name)
-        Bank Name: {{ $prospect->bank_name }}
+    Bank Name: {{ $prospect->bank_name }}
     @endif
     @if ($prospect->website_url)
-        Website URL: {{ $prospect->website_url }}
+    Website URL: {{ $prospect->website_url }}
     @endif
     @if (!empty($prospect->programming_languages))
-        Programming Languages:
+    Programming Languages:
         {{-- {{ is_array($prospect->programming_languages) ? implode(', ', $prospect->programming_languages) : $prospect->programming_languages }} --}}
         {{ is_array($prospect->programming_languages) ? implode(', ', $prospect->programming_languages) : implode(', ', json_decode($prospect->programming_languages, true)) }}
 
