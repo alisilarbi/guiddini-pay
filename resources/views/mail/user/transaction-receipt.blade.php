1813eb50a86f4848e80754fb3fbd75dcae6574ee
da-ci <ali.silarbi00@gmail.com>
2025-04-16 11:44:48 +0100

    still working on email

--- resources/views/mail/user/transaction-receipt.blade.php
+++ resources/views/mail/user/transaction-receipt.blade.php
@@ -285,7 +285,7 @@
                                                                                 style="text-decoration: none;"><img
                                                                                     class="icon" alt="Beefree Logo"
                                                                                     src="{{ asset('images/icon.svg') }}"
-                                                                                    height="auto" width="34"
+                                                                                    height="auto" width="22"
                                                                                     align="center"
                                                                                     style="display: block; height: auto; margin: 0 auto; border: 0;"></a>
                                                                         </td>
