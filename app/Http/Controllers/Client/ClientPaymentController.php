305ba9e45ca8d19a2c9816101395a60f6315f969
da-ci <ali.silarbi00@gmail.com>
2025-04-20 17:41:36 +0100

    bulds

--- app/Http/Controllers/Client/ClientPaymentController.php
+++ app/Http/Controllers/Client/ClientPaymentController.php
@@ -65,6 +65,8 @@ class ClientPaymentController extends Controller
                 // 'gateway_code' => $this->getGatewayErrorCode($gatewayResponse)
             ]);
 
+
+            dd($transaction->application);
             if($transaction->origin === 'System')
                 return redirect()->route('certification', [
                     'slug' => $transaction->slug,
