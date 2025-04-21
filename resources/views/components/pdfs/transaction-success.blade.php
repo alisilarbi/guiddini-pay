b38278792b08a9f289029867641152082adc1751
da-ci <ali.silarbi00@gmail.com>
2025-04-21 19:50:01 +0100

    finishing up

--- resources/views/components/pdfs/transaction-success.blade.php
+++ resources/views/components/pdfs/transaction-success.blade.php
@@ -191,8 +191,8 @@
                 src="{{ public_path('images/cib_logotype.png') }}">
 
             </div>
-            <img style="width: 100%; max-width: 75px ;height: 75px;float: right;"
-                src="{{ url($application->logo)  }}">
+            {{-- <img style="width: 100%; max-width: 75px ;height: 75px;float: right;"
+                src="{{ url($application->logo)  }}"> --}}
     </header>
     <main>
         <div id="invoice-header">
