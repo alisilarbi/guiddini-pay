54b2ad7dbf5d24848c779b8de31d067d340a34e3
da-ci <ali.silarbi00@gmail.com>
2025-07-15 12:24:14 +0100

    new build files

--- resources/views/filament/partner/pages/applications.blade.php
+++ resources/views/filament/partner/pages/applications.blade.php
@@ -3,12 +3,12 @@
         role="alert" tabindex="-1" aria-labelledby="hs-with-description-label">
         <div class="flex">
             <div class="shrink-0">
-                <svg class="shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                <svg class="shrink-0 size-4 mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                     stroke-linejoin="round">
-                    <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
-                    <path d="M12 9v4"></path>
-                    <path d="M12 17h.01"></path>
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <path d="M12 16v-4"></path>
+                    <path d="M12 8h.01"></path>
                 </svg>
             </div>
             <div class="ms-4">
--- resources/views/filament/partner/pages/licenses.blade.php
+++ resources/views/filament/partner/pages/licenses.blade.php
@@ -3,12 +3,12 @@
         role="alert" tabindex="-1" aria-labelledby="hs-with-description-label">
         <div class="flex">
             <div class="shrink-0">
-                <svg class="shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                <svg class="shrink-0 size-4 mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                     stroke-linejoin="round">
-                    <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
-                    <path d="M12 9v4"></path>
-                    <path d="M12 17h.01"></path>
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <path d="M12 16v-4"></path>
+                    <path d="M12 8h.01"></path>
                 </svg>
             </div>
             <div class="ms-4">
@@ -16,7 +16,8 @@
                     N.B :
                 </h3> --}}
                 <div class="text-sm text-primary-700">
-                    Chaque application représente une intégration de paiement prête à être utilisée sur le site web, l'application mobile ou toute autre plateforme numérique de votre client
+                    Chaque application représente une intégration de paiement prête à être utilisée sur le site web,
+                    l'application mobile ou toute autre plateforme numérique de votre client
                 </div>
             </div>
         </div>
